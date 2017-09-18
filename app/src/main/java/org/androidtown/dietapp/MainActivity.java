c5714e94fb3597de9dfd3dbf2004c29ef8fc22f1
JungHunDesk <azxca1731@naver.com>
2017-09-18 23:47:57 +0900

    그래들 파일 수정입니다.

--- app/src/main/java/org/androidtown/dietapp/MainActivity.java
+++ app/src/main/java/org/androidtown/dietapp/MainActivity.java
@@ -1,13 +1,9 @@
 package org.androidtown.dietapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
