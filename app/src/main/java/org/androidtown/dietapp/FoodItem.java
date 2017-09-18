2e790e3187d51601ce3dde8a5ed738b8c7c222ef
JungHunDesk <azxca1731@naver.com>
2017-09-19 01:11:31 +0900

    기본 인증 레이아웃 및 이미지 파일

---
+++ app/src/main/java/org/androidtown/dietapp/FoodItem.java
@@ -0,0 +1,76 @@
+package org.androidtown.dietapp;
+
+/**
+ * Created by latitude7275 on 2017-09-14.
+ */
+
+public class FoodItem {
+    public String category;
+    public String name;
+    public int calorie;
+    public int fat;
+    public int carbohydrate;
+    public int protein;
+
+    public FoodItem() {
+
+
+    }
+
+    public FoodItem(String category, String name,int calorie, int carbohydrate, int protein, int fat) {
+        this.category = category;
+        this.name = name;
+        this.fat = fat;
+        this.carbohydrate = carbohydrate;
+        this.protein = protein;
+        this.calorie = calorie;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCalorie() {
+        return calorie;
+    }
+
+    public void setCalorie(int calorie) {
+        this.calorie = calorie;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public int getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(int carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
+}
